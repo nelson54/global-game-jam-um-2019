e816729a146806f7fc86fe7cc6999bd4383990b8
derek <dereknelson01@gmail.com>
2019-01-24 18:40:50 -0500

    fixed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,20 @@
-var path = require('path')
-var webpack = require('webpack')
-var HtmlWebpackPlugin = require('html-webpack-plugin')
-var BrowserSyncPlugin = require('browser-sync-webpack-plugin')
-
-// Phaser webpack config
-var phaserModule = path.join(__dirname, '/node_modules/phaser-ce/')
-var phaser = path.join(phaserModule, 'build/custom/phaser-split.js')
-var pixi = path.join(phaserModule, 'build/custom/pixi.js')
-var p2 = path.join(phaserModule, 'build/custom/p2.js')
-
-var definePlugin = new webpack.DefinePlugin({
+let path = require('path');
+let webpack = require('webpack');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+let phaserModule = path.join(__dirname, '/node_modules/phaser-ce/');
+let phaser = path.join(phaserModule, 'build/custom/phaser-split.js');
+let pixi = path.join(phaserModule, 'build/custom/pixi.js');
+let p2 = path.join(phaserModule, 'build/custom/p2.js');
+let definePlugin = new webpack.DefinePlugin({
     __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true'))
-})
+});
 
 module.exports = {
     entry: {
         app: [
             'babel-polyfill',
-            path.resolve(__dirname, 'src/main.js')
+            path.resolve(__dirname, 'src/index.js')
         ],
         vendor: ['pixi', 'p2', 'phaser', 'webfontloader']
     },
@@ -77,4 +74,4 @@ module.exports = {
             'p2': p2
         }
     }
-}
+};
