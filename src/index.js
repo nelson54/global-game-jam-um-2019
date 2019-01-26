782f38a3f74f06d1f9817f4fce2e9105dff30725
Dalseides <dalseides@users.noreply.github.com>
2019-01-25 21:27:58 -0500

    Oops. Re-spaced the wrong thing.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ let Phaser = require('phaser-ce');
 let Gameplay = require('./gameplay');
 
 class JamGame extends Phaser.Game {
-    constructor() {
-      super(1024, 768, Phaser.AUTO, document.querySelector("body"), null);
-      this.state.add("Gameplay", Gameplay, true);
+  constructor() {
+    super(1024, 768, Phaser.AUTO, document.querySelector("body"), null);
+    this.state.add("Gameplay", Gameplay, true);
   }
 }
 
