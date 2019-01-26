8b958a9019daab8ef7c51f421d5c34eb6db5791d
Chris Foster <cdbfoster@gmail.com>
2019-01-25 20:05:07 -0500

    Reference the correct DOM object.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-console.log("it works");
-
 let Phaser = require('phaser-ce');
 let Gameplay = require('./gameplay');
 
 class JamGame extends Phaser.Game {
     constructor() {
-    super(800, 600, Phaser.AUTO, "content", null);
+    super(800, 600, Phaser.AUTO, document.querySelector("body"), null);
     this.state.add("Gameplay", Gameplay, true);
   }
 }
 
 let game = new JamGame();
-
