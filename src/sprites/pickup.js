0ea09e382813ca4f4298dbc42ac3dc15eb4a9b07
Dave Odell <dmo2118@gmail.com>
2019-01-26 15:27:27 -0500

    Pickup picked up.

--- src/sprites/pickup.js
+++ src/sprites/pickup.js
@@ -7,17 +7,22 @@ class Pickup extends Phaser.Sprite {
 
     this.scale.set(2);
     this.item = new Gun(this.game, 'normal-bullet');
+    this.players = players; // TODO: Use a group!!!
+    this.enableBody = true;
 
-    for (let player of players) {
-      this.game.physics.arcade.overlap(player, this, this.collect, null, this.game);
-    }
-
+    game.physics.arcade.enable(this);
     game.add.existing(this)
   }
 
   collect(player, pickup) {
     player.weapon = pickup.item;
-    pickup.kill;
+    pickup.kill();
+  }
+
+  update() {
+    for (let player of this.players) {
+      this.game.physics.arcade.overlap(player, this, this.collect, null, this.game);
+    }
   }
 }
 
