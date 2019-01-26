bdeae7e9ba6914f544ce40ee6c97d3accdd8deed
Dalseides <dalseides@users.noreply.github.com>
2019-01-25 19:57:27 -0500

    Adding skeleton. Shouldn't be working yet, but puts a frame to build from.

---
+++ src/gameplay.js
@@ -0,0 +1,12 @@
+var Phaser = require("phaser");
+
+class Gameplay extends Phaser.State {
+  preload() {
+  
+  }
+
+  create() {
+  
+  }
+}
+
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 console.log("it works");
 
 let Phaser = require('phaser-ce');
-
-
-
+let Gameplay = require('./gameplay');
 
 class JamGame extends Phaser.Game {
-
+    constructor() {
+    super(800, 600, Phaser.AUTO, "content", null);
+    this.state.add("Gameplay", Gameplay, true);
+  }
 }
+
+let game = new JamGame();
+
