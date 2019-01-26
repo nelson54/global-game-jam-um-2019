30ef71d31a517d5c2f2e0dc9e693fa1c979f1fe1
Dave Odell <dmo2118@gmail.com>
2019-01-25 21:43:19 -0500

    Big ol' bed.

--- src/gameplay.js
+++ src/gameplay.js
@@ -6,9 +6,15 @@ class Gameplay extends Phaser.State {
   preload() {
     this.game.load.image('player-1', '/assets/sprites/player-1.png');
     this.game.load.image('normal-bullet', '/assets/sprites/normal-bullet.png');
+    this.game.load.image('bed', 'assets/sprites/bed.png');
   }
 
   create() {
+    // this.game.physics.startSystem(Phaser.Physics.ARCADE);
+    var bed = this.game.add.sprite(20, 20, "bed");
+    // bed.enableBody = true;
+    // bed.body.immovable = true;
+
     this.player1 = new Player(this.game, this.game.world.centerX, this.game.world.centerY, 'player-1');
 
     this.player1.weapon = new Gun(this.game, 'normal-bullet');
